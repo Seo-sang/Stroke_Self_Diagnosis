1949438c991e70fb888b219aa8f786ecb50edfae
yongdori00 <yongdori98@gmail.com>
2022-05-09 00:31:04 +0900

    deleted useless

--- stroke/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-
-class ImageUploadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ImageUploadModel
-        fields = '__all__'
--- stroke/urls.py
+++ stroke/urls.py
@@ -6,7 +6,4 @@ from . import views
 
 urlpatterns = [
     path("", views.ReactAppView.as_view()),
-    #path('', views.create, name = 'main'),              #여기서 부터
-    #path('show_train_result/', views.show_train_result, name='show_train_result'),
-    #path('show_result/', views.show_result, name='show_result')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
